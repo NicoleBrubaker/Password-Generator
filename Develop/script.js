8c425555a8f8e5f498e15416509078ce06baa21c
Nicole Brubaker <nschumacher196@gmail.com>
2024-01-16 10:56:49 -0700

    pushing code updates

---
+++ Develop/script.js
@@ -0,0 +1,180 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//generate password function - gathers all password preferences, creates one large arrays based on selections, cuts it down and randomizes based on length defined by user
+var generatePassword = function () {
+  var startPasswordCreation = confirm("Lets define your password!");
+  if (!startPasswordCreation) {
+    return;
+  }
+
+  //asking password length
+  var userPasswordLength = prompt(
+    "Password length: \n*must be 8-128 characters*"
+  );
+  if (!userPasswordLength) {
+    return;
+  }
+  console.log(userPasswordLength);
+
+  //asking for lowercase letters
+  var useLowerCase = confirm("Use lowercase letters?");
+  console.log(useLowerCase);
+
+  //asking for uppercase letters
+  var useUpperCase = confirm("Use uppercase letters?");
+  console.log(useUpperCase);
+
+  //asking for numeric values
+  var useNumValues = confirm("Use numeric values?");
+  console.log(useNumValues);
+
+  //asking for special characters
+  var useSpecialChar = confirm("Use special characters?");
+  console.log(useSpecialChar);
+
+  //all arrays with all possible characters
+
+  //lowercase alphabet array
+  lowerCaseAlphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  //uppercase alphabet array
+  upperCaseAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  //number options array
+  numericValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  //special character options array
+  specialCharacters = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+
+  var userCharacterChoices = [];
+
+  if (useLowerCase === true) {
+    userCharacterChoices = userCharacterChoices.concat(lowerCaseAlphabet);
+  }
+  if (useUpperCase === true) {
+    userCharacterChoices = userCharacterChoices.concat(upperCaseAlphabet);
+  }
+  if (useNumValues === true) {
+    userCharacterChoices = userCharacterChoices.concat(numericValues);
+  }
+  if (useSpecialChar === true) {
+    userCharacterChoices = userCharacterChoices.concat(specialCharacters);
+  }
+  console.log(userCharacterChoices);
+
+  //empty variable to use for below function
+  var completePassword = "";
+
+  //loops through the full character array as many times as determined by userPasswordLength, and concatenates determined values to give the completePassword
+  for (var i = 1; i <= userPasswordLength; i++) {
+    var randomCharacter = randomValue(userCharacterChoices);
+    completePassword = completePassword.concat(randomCharacter);
+  }
+
+  return completePassword;
+};
+
+//determines a random value from a provided array
+var randomValue = function (characterArray) {
+  var getRandomValue = Math.floor(Math.random() * characterArray.length);
+  return characterArray[getRandomValue];
+};
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
